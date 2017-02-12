97dbf51da01539f9d60f9277415a337ae69e8762
Peter Strand <peter@nena.se>
2017-02-12 01:00:48 -0500

    Add simple project that shows a reference image

---
+++ WideColors/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WideColors
+//
+//  Created by Peter Strand on 2017-02-12.
+//  Copyright © 2017 Nena Innovation AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var coreGraphicsView: UIView!
+  @IBOutlet weak var referenceImage: UIImageView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
