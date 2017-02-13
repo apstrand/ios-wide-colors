02d6021bf89095755c44d5e84afe3e551311e4af
Peter Strand <peter@nena.se>
2017-02-12 19:08:15 -0500

    add CoreGraphicsView for custom rendering

---
+++ WideColors/CoreGraphicsView.swift
@@ -0,0 +1,16 @@
+//
+//  CoreGraphicsView.swift
+//  WideColors
+//
+//  Created by Peter Strand on 2017-02-12.
+//  Copyright © 2017 Nena Innovation AB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CoreGraphicsView: UIView {
+  
+  
+  
+}
