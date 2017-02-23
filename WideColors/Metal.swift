f525052d219b6af60e305bf2cbfd257e1cdadeaa
Peter Strand <peter@nena.se>
2017-02-23 11:42:44 -0500

    add dummy Metal.swift file

---
+++ WideColors/Metal.swift
@@ -0,0 +1,24 @@
+//
+//  Metal.swift
+//  WideColors
+//
+//  Created by Peter Strand on 2017-02-21.
+//  Copyright © 2017 Nena Innovation AB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Metal
+
+
+class Metal {
+  
+  var view: UIView
+  
+  init(_ view: UIView) {
+    self.view = view
+  }
+  
+  
+}
+
