257d29cae4158e581a9e65bcf2689da31b4daf04
Peter Strand <peter@nena.se>
2017-02-23 10:51:58 -0500

    support some different CoreGraphics drawing methods

--- WideColors/CoreGraphicsView.swift
+++ WideColors/CoreGraphicsView.swift
@@ -13,12 +13,57 @@ class CoreGraphicsView: UIView {
   
   var image: CGImage?
   
-  override func draw(_ rect: CGRect) {
-    let ctx = UIGraphicsGetCurrentContext()
-    
-    if let img = self.image {
-      ctx?.draw(img, in: rect)
+  enum DrawMethod {
+    case Immediate
+    case NarrowImage
+    case WideImage
+  }
+  var drawMethod: DrawMethod = .Immediate
+  
+  override func draw(_ drawRect: CGRect) {
+    guard let ctx = UIGraphicsGetCurrentContext() else {
+      return
     }
+
+    guard let img = self.image else {
+      ctx.setFillColor(UIColor.red.cgColor)
+      ctx.fill(drawRect)
+      return
+    }
+
+    let imgRect = CGRect(origin: CGPoint.zero, size: CGSize(width: img.width, height: img.height))
+    
+    switch drawMethod {
+    case .Immediate:
+      ctx.draw(img, in: drawRect)
+    case .NarrowImage:
+
+      UIGraphicsBeginImageContextWithOptions(drawRect.size, true, 1.0)
+      if let subctx = UIGraphicsGetCurrentContext() {
+        
+        subctx.draw(img, in: imgRect)
+        
+        if let drawImage = UIGraphicsGetImageFromCurrentImageContext()?.cgImage {
+          ctx.draw(drawImage, in: drawRect)
+        }
+      }
+      UIGraphicsEndImageContext();
+      
+      break
+    case .WideImage:
+      let fmt = UIGraphicsImageRendererFormat()
+      fmt.prefersExtendedRange = true
+      let renderer = UIGraphicsImageRenderer(size: drawRect.size, format: fmt)
+      
+      if let subimg = renderer.image(actions: { ctx in
+        ctx.cgContext.draw(img, in: imgRect)
+        return
+      }).cgImage {
+        ctx.draw(subimg, in: drawRect)
+      }
+      break
+    }
+    
     
   }
   
